cd9eb3f2f78c631a4d95a25febcc046340ae3c1b
jfraught <fraughtdawg@gmail.com>
2024-02-19 08:09:30 -0700

    Initial Commit

---
+++ SocialMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialMedia
+//
+//  Created by Jordan Fraughton on 2/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
